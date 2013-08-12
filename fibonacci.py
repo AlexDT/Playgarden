2bfb391954ce7f9703c5e23cc04a490574bea074
AlexDT <ajdiasteixeira@gmail.com>
2013-08-12 02:26:24 +0200

    Corrected calculation
    - Corrected calculations
    - editted first print line

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,6 @@
 fibnumb = raw_input("Welk getal in de fibonaccireeks wil je zien?\n\n")
 
-print "\n" + str(fibnumb) + " it is!\n"
+print "\n" + str(fibnumb), "it is!\n"
 
 
 def fibonacci(fibnumb):
@@ -10,10 +10,12 @@ def fibonacci(fibnumb):
     for i in range(0, int(fibnumb)):
         if b == 0:
             b = 1
+            print str(i + 1) + ":", str(b)
         else:
+            c = a + b
             a = b
             b = c
-        c = a + b
+            print str(i + 1) + ":", str(c)
     return c
 
 number = fibonacci(fibnumb)
