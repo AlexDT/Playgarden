ca01cdee2b37317846a5cc1eed6854afa435bf85
AlexDT <ajdiasteixeira@gmail.com>
2013-09-09 03:13:24 +0200

    small update

--- Project_Euler/001_sum.py
+++ Project_Euler/001_sum.py
@@ -1,17 +1,17 @@
-'''
-    ONLY READ THIS IF YOU HAVE ALREADY SOLVED THIS PROBLEM!
-    File created for http://projecteuler.net/
-
-    Created by: Alex Dias Teixeira
-    Name:       001_sum.py 
-    Date:       06 Sept 2013
-
-    Problem:    [1] - Multiples of 3 and 5
-        If we list all the natural numbers below 10 that are multiples of 3 or 5,
-        we get 3, 5, 6 and 9. The sum of these multiples is 23.
-        
-        Find the sum of all the multiples of 3 or 5 below 1000.
-'''
+#
+#    ONLY READ THIS IF YOU HAVE ALREADY SOLVED THIS PROBLEM!
+#    File created for http://projecteuler.net/
+#
+#    Created by: Alex Dias Teixeira
+#    Name:       001_sum.py 
+#    Date:       06 Sept 2013
+#
+#    Problem:    [1] - Multiples of 3 and 5
+#        If we list all the natural numbers below 10 that are multiples of 3 or 5,
+#        we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#        
+#        Find the sum of all the multiples of 3 or 5 below 1000.
+#
 
 som = 0
 
--- Project_Euler/003_prime.py
+++ Project_Euler/003_prime.py
@@ -1,13 +1,14 @@
-'''
-    ONLY READ THIS IF YOU HAVE ALREADY SOLVED THIS PROBLEM!
-    File created for http://projecteuler.net/
+#
+#    ONLY READ THIS IF YOU HAVE ALREADY SOLVED THIS PROBLEM!
+#    File created for http://projecteuler.net/
+#
+#    Created by: Alex Dias Teixeira
+#    Name:       003_prime.py
+#    Date:       
+#
+#    Problem:    [3] - Largest prime factor
+#        The prime factors of 13195 are 5, 7, 13 and 29.
+#        
+#        What is the largest prime factor of the number 600851475143 ?
+#
 
-    Created by: Alex Dias Teixeira
-    Name:       003_prime.py
-    Date:       
-
-    Problem:    [3] - Largest prime factor
-        The prime factors of 13195 are 5, 7, 13 and 29.
-        
-        What is the largest prime factor of the number 600851475143 ?
-'''
