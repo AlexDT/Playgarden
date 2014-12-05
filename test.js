11e001f3dbfa242361ce56b186c299af9d1fcf71
AlexDT <ajdiasteixeira@gmail.com>
2014-12-05 12:43:31 +0100

    Removed some files

--- test.js
+++
@@ -1,3 +0,0 @@
-(functino()) {
-    var name = 'Alex';
-})();
--- test/Playgarden/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns Playgarden.core-test
-  (:use clojure.test
-        Playgarden.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
